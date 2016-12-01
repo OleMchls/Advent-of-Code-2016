2325238469755ea69b41933da96cea84e4614825
Ole Michaelis <Ole.Michaelis@googlemail.com>
2016-12-01 23:00:48 +0100

    day 1 - part 2

--- lib/day_1.ex
+++ lib/day_1.ex
@@ -1,10 +1,17 @@
 defmodule Aoc.DayOne do
 
-  defstruct [direction: :north, x: 0, y: 0]
+  defstruct [direction: :north, x: 0, y: 0, history: []]
 
   def distance(directions) do
     parse(directions)
     |> walk(%Aoc.DayOne{})
+    |> calculate_distance
+  end
+
+  def first_duplicate_distance(directions) do
+    parse(directions)
+    |> walk(%Aoc.DayOne{})
+    |> find_first_duplicate(nil)
     |> calculate_distance
   end
 
@@ -39,16 +46,23 @@ defmodule Aoc.DayOne do
 
   defp walk([], state), do: state
 
+  defp progress(state, 0), do: state
   defp progress(state, distance) do
+    state = %{state | history: state.history ++ [{state.x, state.y}] }
     case state.direction do
-      :north -> %{state | y: state.y + distance }
-      :east -> %{state | x: state.x + distance }
-      :south -> %{state | y: state.y - distance }
-      :west -> %{state | x: state.x - distance }
+      :north -> %{state | y: state.y + 1 }
+      :east -> %{state | x: state.x + 1 }
+      :south -> %{state | y: state.y - 1 }
+      :west -> %{state | x: state.x - 1 }
     end
+    |> progress(distance - 1)
   end
 
-  defp calculate_distance(state) do
-    abs(state.x) + abs(state.y)
+  defp find_first_duplicate(%Aoc.DayOne{history: [current | history]} = state, nil) do
+    find_first_duplicate(%{state | history: history }, Enum.find(history, &(&1 == current)))
   end
+  defp find_first_duplicate(_, match), do: match
+
+  defp calculate_distance(%Aoc.DayOne{} = state), do: calculate_distance({state.x, state.y})
+  defp calculate_distance({x, y}), do: abs(x) + abs(y)
 end
